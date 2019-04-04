114b363b64ee940b13e902b2514ded4d2dd0e5c8
joheras <joheras@gmail.com>
2019-04-04 13:21:40 +0200

    Moved xml file

--- src/main/java/ijopencv/examples/FaceDetectionJ_.java
+++ src/main/java/ijopencv/examples/FaceDetectionJ_.java
@@ -5,7 +5,6 @@ package ijopencv.examples;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.Roi;
@@ -42,25 +41,13 @@ public class FaceDetectionJ_ implements Command {
 
     private Object opencv_objdetect;
 
- 
-
     @Override
     public void run() {
-        File file = new File("haarcascade_frontalface_alt.xml");
-        if (!file.exists()) {
-            try {
-                InputStream link = this.getClass().getResourceAsStream("/resources/haarcascade_frontalface_alt.xml");
-                Files.copy(link, file.getAbsoluteFile().toPath());
-            } catch (IOException ex) {
-                Logger.getLogger(FaceDetectionJ_.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
         //Converters
         ImagePlusMatConverter ic = new ImagePlusMatConverter();
         MatImagePlusConverter mip = new MatImagePlusConverter();
         RectRoiConverter rc = new RectRoiConverter();
-        opencv_core.Mat img2 = ic.convert(imp,opencv_core.Mat.class);
-
+        opencv_core.Mat img2 = ic.convert(imp, opencv_core.Mat.class);
 
         // Detect the faces and store them as an array of rectangles
         opencv_core.RectVector rv = detectFaces(img2);
@@ -69,7 +56,7 @@ public class FaceDetectionJ_ implements Command {
         RoiManager rm = new RoiManager();
         rm.setVisible(true);
         for (int i = 0; i < rv.size(); i++) {
-            Roi r = rc.convert(rv.get(i),Roi.class);
+            Roi r = rc.convert(rv.get(i), Roi.class);
             rm.add(imp, r, 0);
         }
 
@@ -79,7 +66,7 @@ public class FaceDetectionJ_ implements Command {
         opencv_core.Mat img_gray = new opencv_core.Mat();
         cvtColor(image, img_gray, CV_BGR2GRAY);
 
-        opencv_objdetect.CascadeClassifier faceclassifier = new opencv_objdetect.CascadeClassifier(IJ.getDirectory("plugins") + "haarcascade_frontalface_alt.xml");
+        opencv_objdetect.CascadeClassifier faceclassifier = new opencv_objdetect.CascadeClassifier(IJ.getDirectory("imagej") + "lib/haarcascade_frontalface_alt.xml");
 
         opencv_core.RectVector rv = new opencv_core.RectVector();
 
